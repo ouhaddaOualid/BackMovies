b573b6113b204e60b06ab488d11c60d2eb3ebc7e
ouhaddaOualid <83510523+ouhaddaOualid@users.noreply.github.com>
2022-11-25 18:51:05 +0100

    back end for web movies

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/projets'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+        '/dist/projets/index.html'));});
+app.listen(process.env.PORT || 8080);
