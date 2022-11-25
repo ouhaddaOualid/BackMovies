d9c1c411fb36c29a8db7eb0d55933af994899758
ouhaddaOualid <83510523+ouhaddaOualid@users.noreply.github.com>
2022-11-25 19:53:30 +0100

    changes 2

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.static(__dirname + '/dist/projets'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+ '/dist/projets/index.html'));});
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
